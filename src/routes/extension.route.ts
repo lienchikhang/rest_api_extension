7e80acb9d11e0eb6cf7a33ead5cf0e99392b3732
lienchikhang <lienchikhang777@gmail.com>
2024-02-08 13:40:06 +0700

    fix api 7

--- src/routes/extension.route.ts
+++ src/routes/extension.route.ts
@@ -15,31 +15,6 @@ router.get('/download', (req: Request, res: Response) => {
     res.setHeader('Content-disposition', 'attachment; filename=folder.zip');
     res.setHeader('Content-type', 'application/zip');
 
-    // try {
-    //     files.forEach(file => {
-    //     const filePath = path.join(folderPath, file);
-    //     const isDirectory = statSync(filePath).isDirectory();
-
-    //     if (!isDirectory) {
-    //         // const fileStream = createReadStream(filePath);
-    //         zip.write(`\r\n--${file}\r\n`);
-    //         zip.write(readFileSync(filePath));
-    //     } else {
-    //         const nestedFiles = readdirSync(filePath);
-    //         nestedFiles.forEach(nestedFile => {
-    //             const nestedFilePath = path.join(filePath, nestedFile);
-    //             const nestedFileStream = createReadStream(nestedFilePath);
-    //             zip.write(`\r\n--${file}/${nestedFile}\r\n`);
-    //             zip.write(readFileSync(nestedFilePath));
-    //         });
-    //     }
-    //     })
-                
-    //     zip.end();
-    //     zip.pipe(res);
-    // } catch (error) {
-    //     console.log(error)
-    // }
 
     try {
         files.forEach(file => {
@@ -53,6 +28,9 @@ router.get('/download', (req: Request, res: Response) => {
             }
         });
 
+         // Đặt tên cho tệp zip khi gửi qua HTTP
+        res.attachment('setup.zip');
+
         // Kết thúc tệp zip
         zip.finalize();
     } catch (error) {
@@ -60,45 +38,6 @@ router.get('/download', (req: Request, res: Response) => {
         res.status(500).send('Internal Server Error');
     }
     
-
-    // readdir(folderPath, (err: NodeJS.ErrnoException | null, files: string[]) => {
-    //     if(err) {
-    //         console.log('error', err);
-    //         return;
-    //     }
-
-    //     files.forEach(file => {
-    //        console.log('file', file);
-    //        const filePath = path.join(folderPath, file);
-    //        console.log('path file', filePath)
-
-    //        //check if there's another folder inside
-    //        const isDirectory = statSync(filePath).isDirectory();
-           
-    //        if (!isDirectory) {
-    //             const stream = createReadStream(filePath, {encoding: 'utf-8'});
-            
-    //             res.setHeader('Content-disposition', 'attachment; filename=' + file);
-    //         // Gửi file dưới dạng luồng
-    //         stream.pipe(res);
-    //        } else {
-    //             readdir(filePath, (err: NodeJS.ErrnoException | null, files: string[]) => {
-    //                 files.forEach(file => {
-    //                     console.log('file', file);
-    //                     const childrenPath = path.join(filePath, file);
-             
-    //                     //check if there's another folder inside
-    //                     const stream = createReadStream(childrenPath, {encoding: 'utf-8'});
-                         
-    //                         res.setHeader('Content-disposition', 'attachment; filename=' + file);
-    //                      // Gửi file dưới dạng luồng
-    //                      stream.pipe(res);
-    //             })
-    //         })
-    //        }
-    //     })
-
-    //     })
 
     })
 
