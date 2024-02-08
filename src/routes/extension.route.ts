e61c0f7437d3277067fdcc081b7145adfecc3e9c
lienchikhang <lienchikhang777@gmail.com>
2024-02-08 12:44:13 +0700

    fix api 2

--- src/routes/extension.route.ts
+++ src/routes/extension.route.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from 'express';
-import { createReadStream, readdir, statSync} from 'fs';
+import { createReadStream, readdir, statSync, mkdir} from 'fs';
+import path from 'path';
 const router = Router();
 
 router.get('/download', (req: Request, res: Response) => {
-    readdir('../setupex', (err: NodeJS.ErrnoException | null, files: string[]) => {
+    const folderPath = path.join('src', 'setupex');
+    readdir(folderPath, (err: NodeJS.ErrnoException | null, files: string[]) => {
         if(err) {
             console.log('error', err);
             return;
@@ -11,16 +13,32 @@ router.get('/download', (req: Request, res: Response) => {
 
         files.forEach(file => {
            console.log('file', file);
+           const filePath = path.join(folderPath, file);
+           console.log('path file', filePath)
 
            //check if there's another folder inside
-           const isDirectory = statSync(`../setupex/${file}`).isDirectory();
+           const isDirectory = statSync(filePath).isDirectory();
            
            if (!isDirectory) {
-                const stream = createReadStream(`../setupex/${file}`, {encoding: 'utf-8'});
+                const stream = createReadStream(filePath, {encoding: 'utf-8'});
             
                 res.setHeader('Content-disposition', 'attachment; filename=' + file);
             // Gửi file dưới dạng luồng
             stream.pipe(res);
+           } else {
+                readdir(filePath, (err: NodeJS.ErrnoException | null, files: string[]) => {
+                    files.forEach(file => {
+                        console.log('file', file);
+                        const childrenPath = path.join(filePath, file);
+             
+                        //check if there's another folder inside
+                        const stream = createReadStream(childrenPath, {encoding: 'utf-8'});
+                         
+                            res.setHeader('Content-disposition', 'attachment; filename=' + file);
+                         // Gửi file dưới dạng luồng
+                         stream.pipe(res);
+                })
+            })
            }
         })
 
